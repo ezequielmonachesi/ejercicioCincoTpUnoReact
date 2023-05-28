044eff702df4191db12cb91727a6d60c020377e3
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-28 00:59:22 -0300

    se corrige warning de consola key

--- src/components/ListaTarea.jsx
+++ src/components/ListaTarea.jsx
@@ -3,13 +3,14 @@ import ItemTarea from "./ItemTarea";
 
 
 const ListaTarea = ({valor}) => {
+    // valor sería el array de Tareas
+    // item es el prop de ItemTarea (creo que así se dice)
   return (
     <ListGroup>
-      {valor.map(e=>{
-        return <ItemTarea item = {e}></ItemTarea>
+      {valor.map((e,i)=>{
+        return <ItemTarea key={i} item = {e}></ItemTarea>
       })}
     </ListGroup>
   );
 };
-
 export default ListaTarea;
